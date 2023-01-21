cb6206c2720db6e462852d94c435b3860456e4db
Apriandisyafiqfirdaus <friangamal@gmail.com>
2023-01-21 10:17:07 +0700

    file UAS NO 7

---
+++ pattern6.java
@@ -0,0 +1,25 @@
+
+
+public class pattern6
+{
+	public static void main(String[] args)
+	{
+		int rows = 4;
+		System.out.println("## Printing the pattern ##");
+		for (int i = 1; i <= rows; i++) 
+                { 
+                        for (int j = rows; j > i; j--)
+			{
+				System.out.print("  ");
+			}
+
+			for (int k = 0; k <= i; k++)
+			{
+				System.out.print (k +" ");
+               
+               
+			}
+			System.out.println();
+		}
+	}
+}
